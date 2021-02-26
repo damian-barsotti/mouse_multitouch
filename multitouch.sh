808e678b3db9cee1212058a730eae96b8807f5bd
Damian Barsotti <damian@famaf.unc.edu.ar>
2021-02-26 20:35:41 -0300

    Add click button and activation button params.

--- multitouch.sh
+++ multitouch.sh
@@ -4,7 +4,8 @@
 
 MOUSE="mouse"
 #MOUSE="touch"
-BUTTON=2
+BUTTON=${3:-1}
+ACT_BUTTON=${4:-2}
 
 DELAY=$1
 
@@ -15,10 +16,10 @@ MOUSE_ID=$(xinput --list | grep -i -m 1 $MOUSE | grep -o 'id=[0-9]\+' | grep -o 
 STATE1=$(xinput --query-state $MOUSE_ID | grep 'button\[' | sort)
 while true; do
     sleep 0.1
-    if xinput --query-state $MOUSE_ID | grep -q "button\[$BUTTON\]=down";
+    if xinput --query-state $MOUSE_ID | grep -q "button\[$ACT_BUTTON\]=down";
     then
         set -x
-        xdotool click --delay $DELAY --repeat $CANT 3
+        xdotool click --delay $DELAY --repeat $CANT $BUTTON
         set +x
     fi
 done
